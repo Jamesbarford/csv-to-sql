261fd60e98a82cd5c907cd0ee83610c6dfb45155
Jamesbarford <jamesbarfordevans@gmail.com>
2021-01-07 22:41:58 +0000

    basic command line handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include "csv_to_sql.hpp"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	std::string query = csv_to_sql("./test-csv/debug.csv", "debug");
-	write_to_file(query, "debug");
+	if (argc < 3)
+	{
+		std::cerr
+			<< "Invalid argument count\n"
+			<< "arg 1 must be the path to the csv \n"
+			<< "arg 2 is the output name i.e table.sql \n";
+	}
+
+	std::string query = csv_to_sql(argv[1], argv[2]);
+	write_to_file(query, argv[2]);
 }
