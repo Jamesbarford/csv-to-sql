9eb4a96d16d3303ba21cf6f26abc2205bcf3ff12
Jamesbarford <jamesbarfordevans@gmail.com>
2021-01-11 11:23:18 +0000

    renamed method

--- src/csv_to_sql.cpp
+++ src/csv_to_sql.cpp
@@ -129,7 +129,7 @@ void csv_to_sql_file(std::string const &path, std::string const &table_name)
 	csv_to_sql_file(path, table_name, type_instruction_map);
 }
 
-std::string csv_to_sql(std::string const &path, std::string const &table_name)
+std::string csv_to_sql_string(std::string const &path, std::string const &table_name)
 {
 	TypeInstructionMap type_instruction_map = create_type_instruction_map_from_sample(path);
 	return csv_to_sql_string(path, table_name, type_instruction_map);
